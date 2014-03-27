8dc3351ef13f3721aba1e15a1c815fc35d501dfe
Chris McDermott <chris.v.mcdermott@gmail.com>
2014-03-27 11:07:01 +0000

    moved the xml fragmanets and xsd

--- src/main/groovy/ConfigGenerator.groovy
+++ src/main/groovy/ConfigGenerator.groovy
@@ -1,9 +1,39 @@
 import groovy.xml.XmlUtil
+import javax.xml.XMLConstants
+import javax.xml.transform.stream.StreamSource
+import javax.xml.validation.SchemaFactory
 
 class ConfigGenerator {
     static void main(String[] args) {
-        def xml = generate(new GroovyShell().parse(new File(args[0])))
-        new File(args[1]).write(XmlUtil.serialize(xml))
+        def xml = generate(new GroovyShell().parse(new File(dslFileName(args))))
+        def xmlString = XmlUtil.serialize(xml)
+
+        if(requiresValidation(args)){
+            validate(xmlString)
+        }
+
+        new File(xmlOutputFileName(args)).write(xmlString)
+    }
+
+    def static dslFileName(args) {
+        args[0]
+    }
+
+    def static xmlOutputFileName(args) {
+        args[1]
+    }
+
+    def static boolean requiresValidation(args) {
+        Boolean.parseBoolean(args[2])
+    }
+
+    private static void validate(String xmlString) {
+        def factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+        def schema = factory.newSchema(new StreamSource(
+                new StringReader(this.getClass().getResource('/genesys8.0.xsd').text)))
+        def validator = schema.newValidator()
+        validator.validate(new StreamSource(
+                new StringReader(xmlString)))
     }
 
     static Node generate(Script dslScript) {
--- src/main/groovy/XmlMixin.groovy
+++ src/main/groovy/XmlMixin.groovy
@@ -1,7 +1,7 @@
 class XmlMixin {
 
     Node readNode(nodeName) {
-        def resource = this.getClass().getResource('/' + nodeName + '.xml')
+        def resource = this.getClass().getResource('/fragments/' + nodeName + '.xml')
         if(resource == null)
             throw new IllegalStateException(String.format("Xml snippet %s.xml not found", nodeName));
         new XmlParser().parseText(resource.text)
